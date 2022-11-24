dbd7adacfbfbe582b33bb51d564127840bdb6f06
LucasHenriqueSL <lucashenrique8736@gmail.com>
2022-11-24 11:05:57 -0300

    dbcontext

---
+++ ProjetoCursoAPI/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoCursoAPI.Models;
+
+namespace ProjetoCursoAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) // está passando o options para a classe base(dbcontext)
+        {
+
+        }
+        //Mapeando entidades
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
+    }
+}
